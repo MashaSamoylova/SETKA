c7ba795c38ccd73beadd4cc89ce086d9a958ecaf
MashaSamoylova <mashanedyak@gmail.com>
2018-11-19 00:22:23 +0700

    screen_refactor

--- screen.py
+++ screen.py
@@ -1,13 +1,13 @@
 from button import Button
 from label import Label
 
-def handler_tab1():
+def handler_tab1_button():
     return 1
 
-def handler_tab2():
+def handler_tab2_button():
     return 2
 
-def handler_tab3():
+def handler_tab3_button():
     return 3
 
 class Screen:
@@ -16,12 +16,12 @@ class Screen:
         self.lcd = lcd
         self.tab_number = init_tab_number
         
-        self.tab1 = Button(lcd, 0, 0, 42, 40, "1")
-        self.tab1.set_handler(handler_tab1)
-        self.tab2 = Button(lcd, 42, 0, 42, 40, "2")
-        self.tab2.set_handler(handler_tab2)
-        self.tab3 = Button(lcd, 84, 0, 42, 40, "3")
-        self.tab3.set_handler(handler_tab3)
+        self.tab1_button = Button(lcd, 0, 0, 42, 40, "1")
+        self.tab1_button.set_handler(handler_tab1_button)
+        self.tab2_button = Button(lcd, 42, 0, 42, 40, "2")
+        self.tab2_button.set_handler(handler_tab2_button)
+        self.tab3_button = Button(lcd, 84, 0, 42, 40, "3")
+        self.tab3_button.set_handler(handler_tab3_button)
         
         self.string1 = Label(lcd, 45, 45, "000.0", 100)
         self.string2 = Label(lcd, 45, 65, "000.0", 100)
@@ -30,17 +30,17 @@ class Screen:
 
     def draw(self):
         if self.tab_number == 1:
-            self.tab1.draw_touched_button()
-            self.tab2.draw()
-            self.tab3.draw()
+            self.tab1_button.draw_touched_button()
+            self.tab2_button.draw()
+            self.tab3_button.draw()
         elif self.tab_number == 2:
-            self.tab1.draw()
-            self.tab2.draw_touched_button()
-            self.tab3.draw()
+            self.tab1_button.draw()
+            self.tab2_button.draw_touched_button()
+            self.tab3_button.draw()
         elif self.tab_number == 3:
-            self.tab1.draw()
-            self.tab2.draw()
-            self.tab3.draw_touched_button()
+            self.tab1_button.draw()
+            self.tab2_button.draw()
+            self.tab3_button.draw_touched_button()
 
         self.string1.draw()
         self.string2.draw()
@@ -50,11 +50,11 @@ class Screen:
     def handler(self):
         touch, x, y = self.lcd.get_touch()
         if touch:
-            if self.tab1.is_touched(x,y):
-                return self.tab1.handler()
-            elif self.tab2.is_touched(x,y):
-                return self.tab2.handler()
-            elif self.tab3.is_touched(x,y):
-                return self.tab3.handler()
+            if self.tab1_button.is_touched(x,y):
+                return self.tab1_button.handler()
+            elif self.tab2_button.is_touched(x,y):
+                return self.tab2_button.handler()
+            elif self.tab3_button.is_touched(x,y):
+                return self.tab3_button.handler()
         return self.tab_number
         
