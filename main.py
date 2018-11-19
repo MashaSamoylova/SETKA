c8ac7f49611f26348b6d65c4ccee816e9dc08c34
MashaSamoylova <mashanedyak@gmail.com>
2018-11-19 23:12:02 +0700

    Fix error on tab

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import time
 
 if __name__=="__main__":
     main_screen.draw()
+    tab1.status_error = True
+    tab1.error_button.text = "Error"
+    current_tab = tab1
+    tab1.draw()
     while True:
         new_tab_number = main_screen.handler()
         if new_tab_number != main_screen.current_tab_number:
@@ -10,6 +14,5 @@ if __name__=="__main__":
             for i, tab in enumerate([tab1, tab2, tab3]):
                 if i == main_screen.current_tab_number:
                     tab.draw()
-                    tab.draw_error()
-        
-        tab2.handler()
+                    current_tab = tab
+        current_tab.handler()
