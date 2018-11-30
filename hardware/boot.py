7ddf9f3970a9181cb7fc31b647e525fa8e6337d1
MashaSamoylova <mashanedyak@gmail.com>
2018-11-30 13:19:49 +0700

    Add makhina

--- hardware/boot.py
+++ hardware/boot.py
@@ -1,30 +1,25 @@
 import lcd160cr
 import micropython
 import machine
-import os
 
 from pyb import Pin
 from pyb import Timer
 
 from ui.screen import Screen
+from makhina.makhina import Makhina
 
 
-#set more detailed information on the exception
+# set more detailed information on the exception
 micropython.alloc_emergency_exception_buf(100)
 
-#lcd
+# lcd
 lcd = lcd160cr.LCD160CR('X')
 lcd.set_orient(lcd160cr.PORTRAIT)
 lcd.set_pen(lcd.rgb(255, 255, 255), lcd.rgb(0, 0, 0))
 lcd.set_brightness(29)
 lcd.erase()
 
-#buttons
-up_switch = machine.Pin("Y1", machine.Pin.IN, machine.Pin.PULL_UP)
-down_switch = machine.Pin("Y2", machine.Pin.IN, machine.Pin.PULL_UP)
-right_switch = machine.Pin("Y3", machine.Pin.IN, machine.Pin.PULL_UP)
-start_switch = machine.Pin("Y4", machine.Pin.IN, machine.Pin.PULL_UP)
-stop_switch = machine.Pin("Y5", machine.Pin.IN, machine.Pin.PULL_UP)
-
-#gui
+# gui
 main_screen = Screen(lcd)
+
+makhina = Makhina()
