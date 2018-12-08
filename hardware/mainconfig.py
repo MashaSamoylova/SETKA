21f509fc073562eb81e05e636fe603078b5884d5
MashaSamoylova <mashanedyak@gmail.com>
2018-12-09 00:49:51 +0700

    asdf

--- hardware/mainconfig.py
+++ hardware/mainconfig.py
@@ -5,24 +5,23 @@ start_button_pin = 'Y11'
 stop_button_pin = 'Y12'
 lcd_pins = 'X'
 
-extruder_pulse_pin = 'Y6'
-first_head_pulse_pin = 'Y6'
-second_head_pulse_pin = 'Y6'
-reciever_pulse_pin = 'Y6'
+extruder_pulse_pin = 'Y3'
+first_head_pulse_pin = 'Y3'
+second_head_pulse_pin = 'Y3'
+reciever_pulse_pin = 'Y3'
 
-motors_enable_pin = 'Y6'
-reciever_enable_pin = 'Y6'
+motors_enable_pin = 'Y3'
+reciever_enable_pin = 'Y3'
 
 uart_pin = 6
-uart_ctrl_pin = 'Y4'
-
+uart_ctrl_pin = 'Y3'
 
 max_temperature = 100
 max_pressure = 100
 
-level_material_pin = "X3"
-break_arm_pin = "X7"
+level_material_pin = "Y3"
+break_arm_pin = "Y3"
 mesh_uart_number = 1
 max_mesh_thickness = 100
 
-emergency_stop_pin = "X8"
+emergency_stop_pin = "Y3"
